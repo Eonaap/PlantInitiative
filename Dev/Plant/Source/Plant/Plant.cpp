b00997d672e31c0b9a8502e6b064178983ecb3de
Eonaap <martijn1dhondt@gmail.com>
2023-02-22 23:40:49 +0100

    Uploading UE5project
    lol

---
+++ Dev/Plant/Source/Plant/Plant.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Plant.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Plant, "Plant" );
---
+++ Dev/Plant/Source/Plant/PlantGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PlantGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLANT_API APlantGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
