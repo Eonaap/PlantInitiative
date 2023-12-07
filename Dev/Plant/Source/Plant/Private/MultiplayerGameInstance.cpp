54709561d7665a99af2c46f0269e904e96288652
Eonaap <martijn1dhondt@gmail.com>
2023-12-07 02:24:03 +0100

    Added presence line

--- Dev/Plant/Source/Plant/Private/MultiplayerGameInstance.cpp
+++ Dev/Plant/Source/Plant/Private/MultiplayerGameInstance.cpp
@@ -97,6 +97,7 @@ void UMultiplayerGameInstance::CreateServer()
 
 	sessionSettings.bShouldAdvertise = true;
 	sessionSettings.bAllowInvites = true;
+	sessionSettings.bUsePresence = true;
 	sessionSettings.NumPublicConnections = 5;
 
 	SessionInterface->CreateSession(0, FName("My session"), sessionSettings);
